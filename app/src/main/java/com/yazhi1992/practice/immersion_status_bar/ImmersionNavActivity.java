0672e8b72796d8b01ae45d7f5bd4ae0f986922f8
zengyazhi <yazhi1992@163.com>
2018-01-10 13:23:01 +0800

    remove lint

--- app/src/main/java/com/yazhi1992/practice/immersion_status_bar/ImmersionNavActivity.java
+++ app/src/main/java/com/yazhi1992/practice/immersion_status_bar/ImmersionNavActivity.java
@@ -1,9 +1,6 @@
 package com.yazhi1992.practice.immersion_status_bar;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -31,15 +28,6 @@ public class ImmersionNavActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
